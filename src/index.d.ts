c26ea7c16b0dcf206d2b593b597e006609eda034
yaochenghao <yaochenghao@bytedance.com>
2018-06-04 22:02:17 +0800

    fix: index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,14 +7,14 @@ export declare class RequestModel {
     constructor(request: IRequestConfig);
 
     public chain(): Chain;
-    public commitWrap(key:string; ...args: any[]): object;
+    public commitWrap(key: string, ...args: any[]): object;
 }
 
 export declare class Chain {
-    constructor(request: Request)
+    constructor(request: Request);
 
-    public commit(key:string; ...args: any[]): Chain;
-    public then(resolve: Function, reject: Function): Chain 
-    public finish(resolve: Function, reject: Function): Chain 
-    public catch(reject: Function): Chain 
+    public commit(key: string, ...args: any[]): Chain;
+    public then(resolve: Function, reject: Function): Chain;
+    public finish(resolve: Function, reject: Function): Chain;
+    public catch(reject: Function): Chain;
 }
