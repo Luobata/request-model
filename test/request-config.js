6e82e28fa642d177f07374c068d261a44a2e45f6
yaochenghao <yaochenghao@bytedance.com>
2018-06-05 08:26:36 +0800

    fix: split rModel init from request to index

--- test/request-config.js
+++ test/request-config.js
@@ -1,59 +1,5 @@
 /* eslint-disable */
 const assert = require('assert');
-const RequestModel = require('../dist/request-model').default;
-const clue = require('./test-model/clue');
-const wrap = require('./test-model/wrap');
-
-const rModel = new RequestModel({
-    state: {},
-    modules: {
-        clue,
-        wrap,
-    },
-    request: {
-        getNameById(params) {
-            // 返回一个Promise
-            // 能不能自动包成一个Promise
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve(params);
-                }, 0);
-            });
-        },
-        enums(params, params2) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve([params, params2]);
-                }, 0);
-            });
-            // 返回一个Promise
-        },
-        enums2(params) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve(params);
-                }, 0);
-            });
-            // 返回一个Promise
-        },
-        enums3(params) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    reject(params);
-                }, 0);
-            });
-            // 返回一个Promise
-        },
-        enums4(params) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve(params);
-                }, 0);
-            });
-            // 返回一个Promise
-        },
-    },
-});
 
 it('config with promiseWrap', function(done) {
     rModel
