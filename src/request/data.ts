c4d7dd6adb9ad20c4ce9e7b6c7cbb225ecb5058e
Luobata <602975805@qq.com>
2018-06-29 23:36:00 +0800

    add: support sub-module api

--- src/request/data.ts
+++ src/request/data.ts
@@ -9,21 +9,24 @@ export const getFunctionInRequest: Function = (
     key: string,
     request: IRequest,
 ): Iresult => {
-    return () => {};
-    // let iRequest: IRequest | Function = request;
-    // if (key.indexOf('/') !== -1) {
-    //     const keys: string[] = key.split('/');
-    //     keys.map(
-    //         (v: string): void => {
-    //             if (!(iRequest instanceof Function)) {
-    //                 iRequest = iRequest[v];
-    //             }
-    //         },
-    //     );
-    // } else {
-    //     iRequest = iRequest[key];
-    // }
-    // if (iRequest instanceof Function) {
-    //     return iRequest;
-    // }
+    let iRequest: IRequest = request;
+    let result: Function;
+    try {
+        if (key.indexOf('/') !== -1) {
+            const keys: string[] = key.split('/');
+            keys.map(
+                (v: string, i: number): void => {
+                    if (i !== keys.length - 1) {
+                        iRequest = iRequest.modules[v];
+                    } else {
+                        result = iRequest.request[v];
+                    }
+                },
+            );
+        } else {
+            result = iRequest.request[key];
+        }
+    } catch (e) {}
+
+    return result;
 };
