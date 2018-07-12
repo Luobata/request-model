a873d0a5f88cfa7974a753707a4ba06640005abf
yaochenghao <yaochenghao@bytedance.com>
2018-07-12 16:20:11 +0800

    remove: console

--- src/request/request.ts
+++ src/request/request.ts
@@ -103,7 +103,6 @@ export default class Request {
         this.requestConfig = request;
 
         this.setting = this.getRequestConfig();
-        console.log(this.setting);
         this.action = this.requestConfig.action;
         this.requestFormat();
     }
@@ -198,7 +197,6 @@ export default class Request {
         );
 
         this.request = outputRequest;
-        console.log(this.request);
     }
 
     private getRequestConfig(): IrequestConfig {
